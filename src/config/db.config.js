56c33d53e190e5c000debea1aab152747d6746ae
Alvaro Bijarra <alvarobijarra14@gmail.com>
2023-06-28 15:38:51 -0300

    config.

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,8 +2,8 @@ const {MONGO_URI, DATABASE, DB_PASSWORD} = require('./enviroment.config.js')
 
 const DB_CONFIG = {
     mongo: {
-        uri: MONGO_URI.replace('<password>', DB_PASSWORD).replace('<database>', DATABASE)
-    }
+        uri: MONGO_URI,
+    },
 }
 
 module.exports = DB_CONFIG 
